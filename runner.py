732506c709942b85e0792f72d2958d21df1ac776
Robert Facella <rob.facella@gmail.com>
2020-09-03 12:47:05 -0400

    Playlist Error Checks URLs Input. Autoplay runs Smoothly between multiple Videos

--- runner.py
+++ runner.py
@@ -1,7 +1,9 @@
 import time
 from selenium import webdriver
 import os
-
+import sys
+#Does not autoplay first video by default, but after initial browser interaction (ie. clicking on the movie player) autoplay works
+first = True
 def testSearch(driver):
     driver.get('http://www.google.com/');
     time.sleep(5) # Let the user actually see something!
@@ -14,18 +16,27 @@ def createWebdriver():
     driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
     return (driver)
 def playVideo(driver, url):
-    driver.get(url)
-    #playlist currently breaks when it reads a BLANK line
-    driver.find_element_by_id("movie_player").click() #Only needed on FIRST video
-    vidStatus = driver.execute_script("return document.getElementById('movie_player').getPlayerState()")
-    while vidStatus != 0:
-       #according to <https://developers.google.com/youtube/js_api_reference?csw=1> state == 0 is when a video has ended
+    try:
+        driver.get(url)
+        global first 
+        if first == True:
+            driver.find_element_by_id("movie_player").click() #Only needed on FIRST video
+            first = False
         vidStatus = driver.execute_script("return document.getElementById('movie_player').getPlayerState()")
-        time.sleep(1) #Wait a second and check again.
+        while vidStatus != 0:
+            #according to <https://developers.google.com/youtube/js_api_reference?csw=1> state == 0 is when a video has ended
+            vidStatus = driver.execute_script("return document.getElementById('movie_player').getPlayerState()")
+            time.sleep(1) #Wait a second and check again.
+    except:
+        print("Encountered an error trying to play from < ") 
+        print (url + "> skipping item." )
+        print ("")
+        sys.stdout.flush()
 def main():
     driver = createWebdriver()
     playlist = open(os.path.join('playlist.txt')).readlines()
     for url in playlist:
         playVideo(driver, url)
+    print ("End of Playlist. Goodbye.")
 main()
 #testSearch(driver)
