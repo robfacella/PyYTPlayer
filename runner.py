342a3f7386cd9856c739b0d963100b5b45f7af75
Robert Facella <rob.facella@gmail.com>
2020-09-03 10:51:29 -0400

    testMethod and createDriver

--- runner.py
+++ runner.py
@@ -1,11 +1,18 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com/');
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+def testSearch(driver):
+    driver.get('http://www.google.com/');
+    time.sleep(5) # Let the user actually see something!
+    search_box = driver.find_element_by_name('q')
+    search_box.send_keys('ChromeDriver')
+    search_box.submit()
+    time.sleep(5)
+    driver.quit()
+def createWebdriver():
+    driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
+    return (driver)
+def main():
+    driver = createWebdriver()
+    #testSearch(driver)
+main()
