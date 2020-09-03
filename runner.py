3a8aacf147a035eebb63cf5295bd087c0087cf32
Robert Facella <rob.facella@gmail.com>
2020-09-03 13:11:28 -0400

    FirefoxTest -how I hate Chrome, oh let me count the ways

--- runner.py
+++ runner.py
@@ -1,8 +1,9 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import Firefox
 import os
 import sys
-#Does not autoplay first video by default, but after initial browser interaction (ie. clicking on the movie player) autoplay works
+#Does not autoplay first video by default, but after initial browser interaction (ie. clicking on the movie player) autoplay works on Chrome but not Firefox?
 first = True
 def testSearch(driver):
     driver.get('http://www.google.com/');
@@ -13,14 +14,16 @@ def testSearch(driver):
     time.sleep(5)
     driver.quit()
 def createWebdriver():
-    driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
+    #driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
+    gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver'))
+    driver = Firefox(executable_path=gecko+'.exe')
     return (driver)
 def playVideo(driver, url):
     try:
         driver.get(url)
         global first 
         if first == True:
-            driver.find_element_by_id("movie_player").click() #Only needed on FIRST video
+            driver.find_element_by_id("movie_player").click() #Only needed on FIRST video, when using chrome. Firefox still click each video...
             first = False
         vidStatus = driver.execute_script("return document.getElementById('movie_player').getPlayerState()")
         while vidStatus != 0:
