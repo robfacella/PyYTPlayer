d55a6503b1e3c49a906a007eaef02f0070859792
Robert Facella <rob.facella@gmail.com>
2020-09-03 13:34:25 -0400

    Switch from Chrome to Gecko

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # PyYTPlayer
 Python Youtube Player
 
-Using ChromeDriver instead of Gecko per the suggestion of a friend.
+Using geckodriver rather than the chromedriver.
+Does not filter ALL ads, but was definately getting more adverts when using chrome.
+
+Was Using ChromeDriver instead of Gecko per the suggestion of a friend.
 	pro:
 	     - just about everything has Chrome OR Chromium installed
+	     - After clicking on the first video, youtube autoplays
 	con:
-	     - does not seem to naturally filter ads on YouTube
+	     - does not naturally filter ads on YouTube
 	     ...and I personally don't like chrome that much...
--- runner.py
+++ runner.py
@@ -1,10 +1,8 @@
 import time
-from selenium import webdriver
 from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 import os
 import sys
-#Does not autoplay first video by default, but after initial browser interaction (ie. clicking on the movie player) autoplay works on Chrome but not Firefox?
-first = True
 def testSearch(driver):
     driver.get('http://www.google.com/');
     time.sleep(5) # Let the user actually see something!
@@ -14,17 +12,20 @@ def testSearch(driver):
     time.sleep(5)
     driver.quit()
 def createWebdriver():
-    #driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
+    #Set path of Driver to the one in Local Dir
     gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver'))
-    driver = Firefox(executable_path=gecko+'.exe')
+    #Generate a FirefoxProfile
+    profile = FirefoxProfile()
+    #for that profile Disable- Web Push Notifications
+    profile.set_preference("permissions.default.desktop-notification", 1)
+    #Create a driver with the above settings
+    driver = Firefox(profile, executable_path=gecko+'.exe')
+    #driver = Firefox(executable_path=gecko+'.exe', profile) #This had args backwards
     return (driver)
 def playVideo(driver, url):
     try:
         driver.get(url)
-        global first 
-        if first == True:
-            driver.find_element_by_id("movie_player").click() #Only needed on FIRST video, when using chrome. Firefox still click each video...
-            first = False
+        driver.find_element_by_id("movie_player").click() #No AutoPlay, smei-Auto workaround
         vidStatus = driver.execute_script("return document.getElementById('movie_player').getPlayerState()")
         while vidStatus != 0:
             #according to <https://developers.google.com/youtube/js_api_reference?csw=1> state == 0 is when a video has ended
