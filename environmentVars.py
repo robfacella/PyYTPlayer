b8da78d82a5f0408f34f7868df7962c03f7469e5
Robert Facella <rob.facella@gmail.com>
2020-09-04 11:09:16 -0400

    1KB a small price to pay for salvation

--- environmentVars.py
+++ environmentVars.py
@@ -1,5 +1,8 @@
 #! python3
 import os
+print ( "[*] Debugging Script: nonintegral to the function of the runner." )
+print ("[*] keeping around as reference tool, PWD may not be ideal path, working for now though")
+print (" ")
 
 for key, val in os.environ.items():
 	print (f' {key}={val}')
