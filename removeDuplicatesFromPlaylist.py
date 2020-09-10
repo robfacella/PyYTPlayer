bf3dcbfdfa6650f6444a653a4783a3b65fa9ea73
Robert Facella <rob.facella@gmail.com>
2020-09-10 10:01:59 -0400

    duplicates removed counter

--- removeDuplicatesFromPlaylist.py
+++ removeDuplicatesFromPlaylist.py
@@ -6,11 +6,14 @@ playlistFile.close()
 newPlaylist = set()
 #unique = []
 unique = 0
+dupes = 0
 for x in playlist:
     if x not in newPlaylist:
         #unique.append(x)
         unique = unique + 1
         newPlaylist.add(x)
+    else:
+        dupes = dupes + 1
 print("Non-duplicate items: " + str(unique))
 #print(unique)
 playlistFile = open(os.path.join('playlist.txt'), 'w')
@@ -18,5 +21,6 @@ for line in newPlaylist:
 	playlistFile.write(line)
 playlistFile.close()
 
-print ("Removed Duplicates.")
+print ("Removed "+ str(dupes) +" Duplicates.")
 print ("[***]Warning, sometimes writes 2 lines together..")
+print ("[??]Have only seen it occur on one line per use, and only when removing duplicate entries; it's probably a relatively easy fix but so is manually scanning the text file of under 200 lines for the line twice as long as the others to split.")
